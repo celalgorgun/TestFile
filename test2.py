4a5564a84f57a1a1d0539ddafd9862fae9d02eff
Celal Gorgun <cgorgun@unomaha.edu>
2017-03-28 22:53:09 -0500

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,12 @@
+class dir:
+    def exercise1():
+    """ We have to numbers and, we multiply these two numbers
+        by each other, and return as total"""
+
+
+        #First Number
+        number1 = 1
+        #Second Number
+        number2 = 5
+        total = number1 * number2
+        print total
