b4570a48cdf733e7584347a5477e86cf49f688fa
Celal Gorgun <cgorgun@unomaha.edu>
2017-03-29 23:14:43 -0500

    Create test6.py

---
+++ test6.py
@@ -0,0 +1,16 @@
+import sys
+
+
+try:
+    fred = [ 4, 5, 11, 43 ]
+    print 'fred =', fred
+    del fred[2]
+    print 'fred =', fred
+    del fred
+    print 'fred =', fred
+    print "Doesn't get here!"
+except NameError, x:
+    print "*** Name", x, "undefined ***"
+
+fred = 'Nimrod'
+print 'fred =', fred
