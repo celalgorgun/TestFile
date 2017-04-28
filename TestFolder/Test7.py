af0130d13214051850607a816ca6e3aa8c87d195
Celal Gorgun <cgorgun@unomaha.edu>
2017-04-27 23:27:53 -0500

    Create Test7.py

---
+++ TestFolder/Test7.py
@@ -0,0 +1,13 @@
+"""Testing for test 7
+dsknvjdknvjkdfnvkjs
+sdknvsjkfdvkjfv"""
+def convertToBinary(n):
+   
+   if n > 1:
+       convertToBinary(n//2)
+   print(n % 2,end = '')
+   
+# decimal number
+dec = 34
+
+convertToBinary(dec)
